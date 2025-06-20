9f18e745186bd9d4d9b53dcaf21f19d44ac4de3d
YankeeTangoMango <yeowtzun@gmail.com>
2025-06-20 11:27:13 +0000

    [automatic commit] 2025-06-20 11:27:13 UTC

--- part1/app.js
+++ part1/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mysql = require('mysql2/promise');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -20,11 +21,7 @@ app.use('/users', usersRouter);
 module.exports = app;
 
 //
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mysql = require('mysql2/promise');
+
 
 var app = express();
 
