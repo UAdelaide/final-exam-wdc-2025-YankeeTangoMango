20ce6e253e8361c67f7c060c9f99c02ef1d49e70
YankeeTangoMango <yeowtzun@gmail.com>
2025-06-20 11:42:33 +0000

    [automatic commit] 2025-06-20 11:42:33 UTC

--- part1/app.js
+++ part1/app.js
@@ -51,7 +51,7 @@ let db;
         password_hash VARCHAR(255) NOT NULL,
         role ENUM('owner','walker') NOT NULL,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-      )
+      );
     `);
 
     // Insert data if table is empty
