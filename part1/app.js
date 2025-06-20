670b2cda6026a04c552b1d98edba70a0e9d057f6
YankeeTangoMango <yeowtzun@gmail.com>
2025-06-20 12:23:47 +0000

    [automatic commit] 2025-06-20 12:23:47 UTC

--- part1/app.js
+++ part1/app.js
@@ -131,6 +131,20 @@ app.get('/', async (req, res) => {
   }
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); */
+
+app.get('/api/dogs', async (req, res) => {
+  try {
+    const [rows] = await db.execute(
+      `SELECT d.name AS dog_name, d.size, u.username AS owner_username
+       FROM Dogs d
+       JOIN Users u ON d.owner_id = u.user_id`
+    );
+    res.json(rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Failed to fetch dogs' });
+  }
+});
 
 module.exports = app;
