488e76bb5f2e6bed5a491e0bb0c658f334041b24
YankeeTangoMango <yeowtzun@gmail.com>
2025-06-21 07:21:20 +0000

    [automatic commit] 2025-06-21 07:21:19 UTC

--- part2/app.js
+++ part2/app.js
@@ -7,6 +7,12 @@ const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'change_this_in_prod',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { httpOnly: true }
++}));
 app.use(express.static(path.join(__dirname, '/public')));
 
 // Routes
